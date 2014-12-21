2bd87fdbb906ee758d7488be9e573476ad957ee3
Josh O. <kaio11602@gmail.com>
2014-12-21 10:41:35 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,9 +16,9 @@ function rotateAnimation(el) {
 }
 window.onload = pre_loader;
 function pre_loader() {
-	document.getElementById('main').style.width=screen.width-"15"+"px";
+	document.getElementById('main').style.width=window.innerWidth-"5"+"px";
 }
 
 window.onresize = function(event) {
-    document.getElementById('main').style.width=screen.width-"15"+"px";
+    document.getElementById('main').style.width=window.innerWidth-"5"+"px";
 }
