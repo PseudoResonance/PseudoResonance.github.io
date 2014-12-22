3106a333e8fb0850c8bf7289857ce5efe1f6acc7
Josh O. <kaio11602@gmail.com>
2014-12-22 09:03:38 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -26,15 +26,27 @@ function pre_loader() {
 			document.getElementById('titletransparent').style.top="110px"
 			document.getElementById('navbar').style.top="120px"
 			document.getElementById('main').style.top="160px"
-			document.getElementById('main|navbar|titletransparent|title').style.width=window.innerWidth-"150"+"px";
-			document.getElementById('main|navbar|titletransparent|title').style.left="60px";
+			document.getElementById('main').style.width=window.innerWidth-"150"+"px";
+			document.getElementById('main').style.left="60px";
+			document.getElementById('navbar').style.width=window.innerWidth-"150"+"px";
+			document.getElementById('navbar').style.left="60px";
+			document.getElementById('titletransparent').style.width=window.innerWidth-"150"+"px";
+			document.getElementById('titletransparent').style.left="60px";
+			document.getElementById('title').style.width=window.innerWidth-"150"+"px";
+			document.getElementById('title').style.left="60px";
 		} else if(window.innerWidth <= "799") {
 			document.getElementById('title').style.top="0px"
 			document.getElementById('titletransparent').style.top="80px"
 			document.getElementById('navbar').style.top="90px"
 			document.getElementById('main').style.top="130px"
-			document.getElementById('main|navbar|titletransparent|title').style.width=window.innerWidth-"30"+"px";
-			document.getElementById('main|navbar|titletransparent|title').style.left="0px";
+			document.getElementById('main').style.width=window.innerWidth-"30"+"px";
+			document.getElementById('main').style.left="0px";
+			document.getElementById('navbar').style.width=window.innerWidth-"30"+"px";
+			document.getElementById('navbar').style.left="0px";
+			document.getElementById('titletransparent').style.width=window.innerWidth-"30"+"px";
+			document.getElementById('titletransparent').style.left="0px";
+			document.getElementById('title').style.width=window.innerWidth-"30"+"px";
+			document.getElementById('title').style.left="0px";
 		}
 	}
 }
@@ -48,15 +60,27 @@ window.onresize = function(event) {
 			document.getElementById('titletransparent').style.top="110px"
 			document.getElementById('navbar').style.top="120px"
 			document.getElementById('main').style.top="160px"
-			document.getElementById('main|navbar|titletransparent|title').style.width=window.innerWidth-"150"+"px";
-			document.getElementById('main|navbar|titletransparent|title').style.left="60px";
+			document.getElementById('main').style.width=window.innerWidth-"150"+"px";
+			document.getElementById('main').style.left="60px";
+			document.getElementById('navbar').style.width=window.innerWidth-"150"+"px";
+			document.getElementById('navbar').style.left="60px";
+			document.getElementById('titletransparent').style.width=window.innerWidth-"150"+"px";
+			document.getElementById('titletransparent').style.left="60px";
+			document.getElementById('title').style.width=window.innerWidth-"150"+"px";
+			document.getElementById('title').style.left="60px";
 		} else if(window.innerWidth <= "799") {
 			document.getElementById('title').style.top="0px"
 			document.getElementById('titletransparent').style.top="80px"
 			document.getElementById('navbar').style.top="90px"
 			document.getElementById('main').style.top="130px"
-			document.getElementById('main|navbar|titletransparent|title').style.width=window.innerWidth-"30"+"px";
-			document.getElementById('main|navbar|titletransparent|title').style.left="0px";
+			document.getElementById('main').style.width=window.innerWidth-"30"+"px";
+			document.getElementById('main').style.left="0px";
+			document.getElementById('navbar').style.width=window.innerWidth-"30"+"px";
+			document.getElementById('navbar').style.left="0px";
+			document.getElementById('titletransparent').style.width=window.innerWidth-"30"+"px";
+			document.getElementById('titletransparent').style.left="0px";
+			document.getElementById('title').style.width=window.innerWidth-"30"+"px";
+			document.getElementById('title').style.left="0px";
 		}
 	}
 }
