a76b36f10b5f68c74df959b6b1e2fd88ae19803e
Josh O. <kaio11602@gmail.com>
2014-12-22 09:00:44 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,8 +21,21 @@ function pre_loader() {
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 		document.getElementById('main').style.width=window.innerWidth-"30"+"px";
 	} else {
-		document.getElementById('main').style.width=window.innerWidth-"150"+"px";
-		document.getElementById('main').style.left="60px";
+		if(window.innerWidth >= "800") {
+			document.getElementById('title').style.top="30px"
+			document.getElementById('titletransparent').style.top="110px"
+			document.getElementById('navbar').style.top="120px"
+			document.getElementById('main').style.top="160px"
+			document.getElementById('main|navbar|titletransparent|title').style.width=window.innerWidth-"150"+"px";
+			document.getElementById('main|navbar|titletransparent|title').style.left="60px";
+		} else if(window.innerWidth <= "799") {
+			document.getElementById('title').style.top="0px"
+			document.getElementById('titletransparent').style.top="80px"
+			document.getElementById('navbar').style.top="90px"
+			document.getElementById('main').style.top="130px"
+			document.getElementById('main|navbar|titletransparent|title').style.width=window.innerWidth-"30"+"px";
+			document.getElementById('main|navbar|titletransparent|title').style.left="0px";
+		}
 	}
 }
 
@@ -31,11 +44,19 @@ window.onresize = function(event) {
 		document.getElementById('main').style.width=window.innerWidth-"30"+"px";
 	} else {
 		if(window.innerWidth >= "800") {
-			document.getElementById('main').style.width=window.innerWidth-"150"+"px";
-			document.getElementById('main').style.left="60px";
+			document.getElementById('title').style.top="30px"
+			document.getElementById('titletransparent').style.top="110px"
+			document.getElementById('navbar').style.top="120px"
+			document.getElementById('main').style.top="160px"
+			document.getElementById('main|navbar|titletransparent|title').style.width=window.innerWidth-"150"+"px";
+			document.getElementById('main|navbar|titletransparent|title').style.left="60px";
 		} else if(window.innerWidth <= "799") {
-			document.getElementById('main').style.width=window.innerWidth-"30"+"px";
-			document.getElementById('main').style.left="0px";
+			document.getElementById('title').style.top="0px"
+			document.getElementById('titletransparent').style.top="80px"
+			document.getElementById('navbar').style.top="90px"
+			document.getElementById('main').style.top="130px"
+			document.getElementById('main|navbar|titletransparent|title').style.width=window.innerWidth-"30"+"px";
+			document.getElementById('main|navbar|titletransparent|title').style.left="0px";
 		}
 	}
 }
